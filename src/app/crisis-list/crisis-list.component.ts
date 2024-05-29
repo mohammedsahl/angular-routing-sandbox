4ef7a64036581e05c49bf8fb9e8f64d394b4bf2d
Mohammed Sahl <msahl@essentialclinic.ca>
2024-05-29 12:02:49 -0600

    Add some logging

--- src/app/crisis-list/crisis-list.component.ts
+++ src/app/crisis-list/crisis-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NavigationStart, Router, RouterLink, Scroll} from "@angular/router";
+import {filter, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-crisis-list',
@@ -10,6 +11,32 @@ import {RouterLink} from "@angular/router";
   templateUrl: './crisis-list.component.html',
   styleUrl: './crisis-list.component.css'
 })
-export class CrisisListComponent {
+export class CrisisListComponent implements OnInit, OnDestroy {
+  private subscription: Subscription | undefined;
 
+  constructor(private readonly router: Router) {}
+
+  ngOnInit(): void {
+    this.subscription = this.router.events
+      .pipe(
+        filter((e) => e instanceof NavigationStart || e instanceof Scroll),
+        filter((e) => {
+          if (e instanceof Scroll) {
+            return true;
+          }
+          return (<NavigationStart>e).navigationTrigger === 'popstate';
+        }),
+      )
+      .subscribe((e) => {
+        if (e instanceof NavigationStart) {
+          console.log('Popped state event: ', e);
+        } else {
+          console.log('Scroll event: ', e);
+        }
+      });
+    }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
